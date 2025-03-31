7a65f2b796cd0554aba25379cc049106775e1b10
zibusisojnduna <zibusisonduna2001@gmail.com>
2025-03-31 15:48:32 +0200

    navigator.js updated, file paths fixed\

--- src/navigation/navigator.js
+++ src/navigation/navigator.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import RegisterScreen from "./src/screens/registerScreen";
+import LoginScreen from "./src/screens/loginScreen";
+import ProfileScreen from "./src/screens/profileScreen";
 import RestaurantList from "./src/screens/restaurantList";
 import ReservationForm from "./src/screens/reservationForm";
 
@@ -10,6 +13,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Restaurants" component={RestaurantList} />
         <Stack.Screen name="ReservationForm" component={ReservationForm} />
       </Stack.Navigator>
